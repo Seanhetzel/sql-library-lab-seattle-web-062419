42bf4d37cf465abbab25293e0ec36175fbfc6ed7
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-08 16:33:48 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,2 +1,3 @@
-CREATE TABLE Series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
-CREATE TABLE sub-genre()
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE subgenres(id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
