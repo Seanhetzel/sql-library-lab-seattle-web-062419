bde606346d4f58201f38036e878edae911ac8f4e
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-08 17:59:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  #{}"SELECT characters.name, COUNT(*) FROM characters JOIN character_books ON characters.id = character_books.character_id;"
   "SELECT characters.name, COUNT(book_id) FROM character_books JOIN characters ON characters.id = character_books.character_id GROUP BY characters.name ORDER BY COUNT(book_id) DESC;"
-  #{}"SELECT characters.name, COUNT(*) as book_count FROM character_books JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY book_count DESC"
 end
